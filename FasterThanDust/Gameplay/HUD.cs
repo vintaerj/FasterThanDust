beb38d9588499a7c8da4b3e8749623cc6645c00f
Jean BAZIN <jean.bazin.etu@univ-lille.fr>
2018-09-26 18:19:43 +0200

    a l'aide

--- FasterThanDust/Gameplay/HUD.cs
+++ FasterThanDust/Gameplay/HUD.cs
@@ -1,12 +1,31 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
 namespace GameJam17.Gameplay
 {
     public class HUD
     {
+
+        public HUD()
+        {
+            
+        }
         
+        // a l'aide !!
+        private void afficherEquipe(){
+            Color data = new Color[300 * 150];
+            Texture2D rectTexture = new Texture2D(GraphicsDevice, 300, 150);
+
+            for (int i = 0; i < data.Length; ++i)
+            {
+                data[i] = Color.White;
+            }
+
+            rectTexture.SetData(data);
+            var position = new Vector2(rectangle.Left, rectangle.Top);
+
+            spriteBatch.Draw(rectTexture, new Vector2(), Color.White);
+        }
     }
     
-    
-//    private void afficherEquipe(){
-//        
-//    }
 }
